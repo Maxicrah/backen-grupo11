c3eb0608c5fed9f0a97a4d217bd83375e2b1b434
MAXII\maxii <maximaxmaxii690@gmail.com>
2024-07-11 22:50:16 -0300

    test

--- route/usuario.route.js
+++ route/usuario.route.js
@@ -8,11 +8,11 @@ router.post('/registro', authCtrl.registerUser);
 router.post('/login', authCtrl.loginUser);
 
 router.get('/usuarios', authCtrl.verifyToken, usuarioCtrl.getAllUsuarios);
-router.post('/', authCtrl.verifyToken, usuarioCtrl.createUsuario);
 router.get('/:id', authCtrl.verifyToken, usuarioCtrl.getUsuarioById);
 router.put('/:id', authCtrl.verifyToken, usuarioCtrl.updateUsuario);
 router.delete('/:id', authCtrl.verifyToken, usuarioCtrl.deleteUsuario);
 
+router.get('/permisos/:id', authCtrl.verifyToken, usuarioCtrl.getPermisosUsuario);
 router.get('/rol/:id', authCtrl.verifyToken, usuarioCtrl.getRolUsuario);
 
 
